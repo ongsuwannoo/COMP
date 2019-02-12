564745e178b0e88fe64a66931904aa20c25d0042
JAKKRAPAT SUWANNO <ongsuwannoo@gmail.com>
2019-02-13 00:40:53 +0700

    13/02/19

--- GDB Lab 1.c
+++ GDB Lab 1.c
@@ -4,10 +4,7 @@ int main() {
     int a = 17, b = 25, c = 50, d = 10;
 
     // Assignment Statements of Each Question
-    d = b % a + c / b;
-    d = b % a++ + c / b;
-    d = b % ++a + c / b;
-    d = b % (a + c) / b;
-
+    a += a++;
+    a += ++a;
     return 0;
 }
