c61b1d0b007398b232a5e396303f5d85fc79b316
61070022 ONG <[ongsuwannoo@gmail.com]>
2019-03-05 20:15:06 +0700

    05/03/19

---
+++ Lab7-3.c
@@ -0,0 +1,26 @@
+// Lab7-3
+#include <stdio.h>
+#include <math.h>
+
+double perimeter(double a, double b)
+{
+    double c;
+    c = hypot(a, b);
+
+    return a+b+c; 
+}
+
+double area(double a, double b)
+{
+    return 0.5*b*a;
+}
+
+int main()
+{
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("Perimeter: %.2lf\n", perimeter(a, b));
+    printf("Area: %.2lf\n", area(a, b));
+
+    return 0;
+}
